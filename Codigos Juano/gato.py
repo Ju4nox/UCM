316e8f3ebefbf4d7c113875bae5f766cd21aded5
Ju4nox <114707186+Ju4nox@users.noreply.github.com>
2022-10-20 16:26:16 -0300

    Create gato.py

---
+++ Codigos Juano/gato.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+
+#tablero
+def tablero():
+    
+    pass
+#lanzar un dado
+#seleccionar jugador
+#marcar las casillas
+
+if __name__=="__main__":
+    
+    pass
