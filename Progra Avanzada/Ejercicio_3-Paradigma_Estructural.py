772ba2fba3a997979127685771aa0fa68ca1e1fa
Ju4nox <114707186+Ju4nox@users.noreply.github.com>
2024-04-02 16:07:32 -0300

    Create Ejercicio_3-Paradigma_Estructural.py

---
+++ Progra Avanzada/Ejercicio_3-Paradigma_Estructural.py
@@ -0,0 +1,77 @@
+"""Un juego de Sudoku se considera correcto si cumple las siguientes condiciones:
+Cada fila y columna contiene los números del 1 al 9 sin repetirse.
+Cada subcuadrícula de 3x3 contiene los números del 1 al 9 sin repetirse.
+
+Desarrolle un programa utilizando el paradigma estructurado que dada una matriz de 9x9, determine si el Sudoku es correcto o no.
+"""
+
+if __name__ == "__main__":
+    sudoku = [[4,1,2,7,6,5,9,3,8],[6,5,8,3,2,9,7,4,1],[9,7,3,8,1,4,6,5,2],
+              [5,7,3,1,9,6,8,2,4],[9,8,2,4,7,5,1,3,6],[4,6,1,2,3,8,5,9,7],
+              [3,5,9,6,4,1,2,8,7],[8,1,4,2,9,7,5,6,3],[7,2,6,3,8,5,1,4,9]]
+    #creacion variable largo de sudoku
+    largo = len(sudoku)
+    #recorrido del sudoku
+    for j in range(largo):
+        #condicion que pregunta si los numeros se repiten
+        if  set(sudoku[j]) == set([1,2,3,4,5,6,7,8,9]):
+            #muestra en pantalla si se repite algún numero en alguna de las subcuadriculas del sudoku, especificando de izquierda a derecha en cuales
+            print("No se repite ningun numero en la cuadricula")
+            #print(set(sudoku[j]), set([1,2,3,4,5,6,7,8,9]))
+        else:
+            print("Se repite algún numero")
+#verifica las primeras 3 filas    
+#crear una lista 
+    lista = []
+#recorrer la sublista
+    for i in range(9):
+#recorrer los numeros dentro de las listas
+        for j in range(3):
+#almacenar los numeros dentro de la lista
+            if len(lista) < 9:
+                lista.append(sudoku[i][j])
+#si es que la lista está llena entonces hay que saber si es igual al set
+                if set(lista) == set([1,2,3,4,5,6,7,8,9]):
+                    print("La fila no repite ningún numero")
+                    #print(lista)
+                    lista.clear()
+                    
+    #print(lista)
+#verifica las siguienes 3 filas
+#crear una lista 
+    lista_two = []
+#recorrer la sublista
+    for i in range(9):
+#recorrer los numeros dentro de las listas
+        for j in range(3):
+#almacenar los numeros dentro de la lista
+            if len(lista_two) < 9:
+                lista_two.append(sudoku[i][j+3])
+#si es que la lista está llena entonces hay que saber si es igual al set
+                if set(lista_two) == set([1,2,3,4,5,6,7,8,9]):
+                    print("La fila no repite ningún numero")
+                    #print(lista_two)
+                    lista_two.clear()
+#verifica las últimas 3 filas
+#crear una lista 
+    lista_three = []
+#recorrer la sublista
+    for i in range(9):
+#recorrer los numeros dentro de las listas
+        for j in range(3):
+#almacenar los numeros dentro de la lista
+            if len(lista_three) < 9:
+                lista_three.append(sudoku[i][j+6])
+#si es que la lista está llena entonces hay que saber si es igual al set
+                if set(lista_three) == set([1,2,3,4,5,6,7,8,9]):
+                    print("La fila no repite ningún numero")
+                    #print(lista_three)
+                    lista_three.clear()
+                    
+    #crear una lista
+    list_c_one = []
+    #recorrer la sublista
+    
+   #recorrer los elementos
+    
+   #agregar a la lista los primeros elementos de cada sublista
