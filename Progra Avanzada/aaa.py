4070b880885dd994102cdaca8896983b87495112
Ju4nox <114707186+Ju4nox@users.noreply.github.com>
2024-04-30 17:49:28 -0400

    aaa.py

--- Progra Avanzada/aaa.py
+++ Progra Avanzada/aaa.py
@@ -1,9 +1,9 @@
 variable = int(input("Ingrese el descuento (no debe ser mayor a 50%)"))
 total = 0
-if a > 50:
+if variable > 50:
     print("Debe ingresar un descuento menor")
 else:
-    descuento = a / 100
+    descuento = variable / 100
 for i in Carrito_compra:
     total += Carrito_compra[i][2]
     
