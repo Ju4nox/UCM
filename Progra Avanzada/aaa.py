deee6fd18eb5e258ee7fd4bc9daa770b9bb5977c
Gustavo Harnisch <138950084+Gustavo-Harnisch@users.noreply.github.com>
2024-04-30 18:41:44 -0400

    Update aaa.py

--- Progra Avanzada/aaa.py
+++ Progra Avanzada/aaa.py
@@ -1,10 +1,14 @@
-Carrito_compra = {}
 def nuevo_dato():
-    id = int(input ("Ingrese el id del producto>>>"))
-    nombre = input("Ingrese el nombre del producto>>>")
-    cantidad = int(input(f"Cuantas unidades del producto {nombre} se compran?>>>"))
-    precio = int(input(f"Ingrese  el precio del {nombre}>>>"))
-    return id, nombre, cantidad, precio
+    try:
+        id = int(input ("Ingrese el id del producto>>>"))
+        nombre = input("Ingrese el nombre del producto>>>")
+        cantidad = int(input(f"Cuantas unidades del producto {nombre} se compran?>>>"))
+        precio = int(input(f"Ingrese  el precio del {nombre}>>>"))
+        return id, nombre, cantidad, precio
+    except ValueError:
+        print("Error de datos no numericos")
+
+
 def agregar(id, nombre, cantidad, precio):
     while True:
         try:
@@ -16,10 +20,14 @@ def agregar(id, nombre, cantidad, precio):
             print("El articulo no se a incluido")
             input("Ingrese  Enter para continuar...")
             break
+
+
 def eliminar(id):
     while True:
         try:
+            nombre = Carrito_compra[id][0]
             del Carrito_compra[id]
+            print(f"El producto \"{nombre}\" fue exitosamente eliminado")
             break
         except Exception as error:
             print("ERROR: \"" + str(error)+"\"")
@@ -31,7 +39,12 @@ def eliminar(id):
 def editar(id):
     while True:
         try:
-            print("NONE CODE SOURCE")
+            for id_lista in Carrito_compra:
+                if id_lista == id:
+                    cantidad = int(input("Corriga la cantidad que desea llevar:"))
+                    Carrito_compra[id][1] = cantidad
+                    print("Correccion exitosa")
+                    break
         except Exception as error:
             print("ERROR: \"" + str(error)+"\"")
             print("El articulo no se a elimindo")
@@ -46,10 +59,11 @@ def mostrar():
         print(f"ID:{info}")
         print(f"Producto:{Carrito_compra[info][0]}")
         print(f"Cantidad:{Carrito_compra[info][1]}")
-        print(f"Precio:${Carrito_compra[info][0]}")
+        print(f"Precio:${Carrito_compra[info][2]}")
         print(f"Total:${Carrito_compra[info][1] * Carrito_compra[info][2]}")
         print("_____________________________________")
         total += Carrito_compra[info][1] * Carrito_compra[info][2]
+    print(f"EL TOTAL ES :${total}")
 
 def pagar(resultado):
     print("***************************************")
@@ -58,52 +72,51 @@ def pagar(resultado):
     
 
 def calcular_porciento():
-    variable = int(input("Ingrese el descuento (no debe ser mayor a 50%)"))
+    descuento = int(input("Ingrese el descuento (no debe ser mayor a 50%)"))
     total = 0
-    if variable > 50:
+    if descuento > 50:
         print("Debe ingresar un descuento menor")
-    else:
-        descuento = variable / 100
     for i in Carrito_compra:
-        total += Carrito_compra[i][2]
-        
-    descuento_aplicado = total*descuento
+        total += Carrito_compra[i][2]*Carrito_compra[i][1]   
+    descuento_aplicado = (total*descuento) /100
     resultado = total-descuento_aplicado
     return resultado
 
 
 def menu():
     while True:
-        print("1) Agregar Producto")
-        print("2) Eliminar Producto")
-        print("3) Editar cantidad de productos")
-        print("4) Ver carrito de compras")
-        print("5) Pagar")
-        print("6) Salir")
-        opcion  = input("Ingrese alguna opción>>>")
-        if opcion   == '1':
-            id, nombre, stock, precio,  = nuevo_dato()
-            agregar(id, nombre, stock, precio)
-        elif opcion == '2':
-            id = int(input("Ingrese el ID producto que desea eliminar "))
-            eliminar(id)
-        elif opcion == '3':
-            id = int(input("Ingrese el ID producto que desea eliminar "))
-            editar(id)
-        elif opcion == '4':
-            mostrar()
-        elif opcion == '5':
-            resultado = calcular_porciento()
-            pagar(resultado)
-            print("Gracias por algo")
-            break
-        elif opcion == '6':
-            break
-        else:
-            print("Esa opción no existe")
+        try:
+            print("1) Agregar Producto")
+            print("2) Eliminar Producto")
+            print("3) Editar cantidad de productos")
+            print("4) Ver carrito de compras")
+            print("5) Pagar")
+            print("6) Salir")
+            opcion  = input("Ingrese alguna opción>>>")
+            if opcion   == '1':
+                id, nombre, stock, precio,  = nuevo_dato()
+                agregar(id, nombre, stock, precio)
+            elif opcion == '2':
+                id = int(input("Ingrese el ID producto que desea Eliminar "))
+                eliminar(id)
+            elif opcion == '3':
+                id = int(input("Ingrese el ID producto que desea Editar "))
+                editar(id)
+            elif opcion == '4':
+                mostrar()
+            elif opcion == '5':
+                resultado = calcular_porciento()
+                pagar(resultado)
+                print("Gracias por comprar aqui!!!")
+                break
+            elif opcion == '6':
+                break
+            else:
+                print("Esa opción no existe")
+        except ValueError:
+            print("Error de valor")
 
-Carrito_compra = {}
 if __name__ == "__main__":
-    #Carrito_compra = {}
+    Carrito_compra = {}
     menu()
 
