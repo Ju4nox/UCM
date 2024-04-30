8c84a6262bfaf05f97f90a99976cefe85c3631e6
Ju4nox <114707186+Ju4nox@users.noreply.github.com>
2024-04-30 17:44:49 -0400

    aaa.py

--- Progra Avanzada/aaa.py
+++ Progra Avanzada/aaa.py
@@ -1,30 +1,8 @@
-# crear el usuario
-
-
-def main(lista):
-    while True:
-        x = int(input("Ingrese alguna opción (utilizando los números)"))
-        print("1) Agregar Producto")
-        print("2) Eliminar Producto")
-        print("3) Editar cantidad de productos")
-        print("4) Pagar")
-        print("5) Salir")
-        if x == 1:
-            agregar()
-        elif x == 2:
-            ingresar()
-        elif x == 3:
-            editar()
-        elif x == 4:
-            pago()
-            print("Gracias por algo")
-            break
-        elif x == 5:
-            break
-        else:
-            print("Esa opción no existe")
-    pass
-
-
-if __name__ == "__main__":
-    lista = []
+a = int(input("Ingrese el descuento (no debe ser mayor a 50%)"))
+total = 0
+if a > 50:
+    print("Debe ingresar un descuento menor")
+else:
+    descuento = a / 100
+for i in Carrito_compra:
+    total += Carrito_compra[i][2]
