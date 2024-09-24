3a88f81aeb708c2571b22b63d7fd6b9d96061308
Ju4nox <114707186+Ju4nox@users.noreply.github.com>
2024-09-24 17:57:30 -0300

    Add files via upload

---
+++ Arqui/bbbbbbbbbb.asm
@@ -0,0 +1,20 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+    mov ax,10
+    mov bx,5
+    
+    add ax,bx
+    sub ax,bx
+    mul bx
+    div bx
+    
+
+ret
+
+
+
+
